e4ef72e00060328406e1d17fea3373a7362e64d6
João Bruno Vasconcelos <joaobruno91@gmail.com>
2022-02-22 00:02:55 -0300

    Cria migration create postCategory

--- migrations/20220222021846-create-postCategory.js
+++ migrations/20220222021846-create-postCategory.js
@@ -1,11 +1,27 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Categories', {
-      id: {
+      postId: {
         allowNull: false,
-        autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'BlogPost',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      categoryId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Category',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
       },
     });
   },
