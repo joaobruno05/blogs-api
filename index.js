aa350b42a973633f9c9c5cc0eceecb488b17c496
João Bruno Vasconcelos <joaobruno91@gmail.com>
2022-02-22 20:19:08 -0300

    Cria rota para cadastrar usuario

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const errorMiddleware = require('./middlewares/errorMiddleware');
+const userRouter = require('./routes/userRouter');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+app.use(express.json());
+
+app.use('/user', userRouter);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.use(errorMiddleware);
+
+app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
