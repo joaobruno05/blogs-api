b27c9851f006c75572fa07accf355ab356bb744c
João Bruno Vasconcelos <joaobruno91@gmail.com>
2022-02-23 15:40:26 -0300

    Refatora a model Category

--- models/Category.js
+++ models/Category.js
@@ -1,11 +1,26 @@
-const Category = (sequelize, DataTypes) => {
-  const category = sequelize.define('Category', {
-    name: DataTypes.STRING,
+const { DataTypes } = require('sequelize');
+
+const Attributes = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
   },
-  {
-    timestamps: false,
-  });
-  return category;
+  name: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+  },
 };
 
-module.exports = Category;
+module.exports = (sequelize) => {
+  const Category = sequelize.define(
+    'Category',
+    Attributes,
+    {
+      timestamps: false,
+      tableName: 'Categories',
+    },
+  );
+  return Category;
+};
