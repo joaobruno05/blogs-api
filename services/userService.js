e090988ee7b7d5c72c98400579005df66980bcf5
João Bruno Vasconcelos <joaobruno91@gmail.com>
2022-02-23 00:44:16 -0300

    Cria funçao para listar todos os usuarios

--- services/userService.js
+++ services/userService.js
@@ -26,6 +26,14 @@ const createUser = async (displayName, email, password, image) => {
   return user;
 };
 
+const getAllUsers = async () => {
+  const users = await User.findAll({
+    attributes: { exclude: ['password'] },
+  });
+  return users;
+};
+
 module.exports = {
   createUser,
+  getAllUsers,
 };
