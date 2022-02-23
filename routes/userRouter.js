ed180c4852fafa98be4643e894d94bec2c42059a
João Bruno Vasconcelos <joaobruno91@gmail.com>
2022-02-23 00:54:59 -0300

    Cria rota GET /user para listar todos os usuarios

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.post('/', userController.create);
+router.get('/', authMiddleware, userController.getAll);
 
 module.exports = router;
